a864accfcffb8c92c58ad8d78d0421300b9cd019
Pankaj Singh <pkstiyara@gmail.com>
2024-01-23 12:36:53 +0000

    infra ready with 4 basic pages

---
+++ gurgaonhometution/home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("This is home page.")
+
+def about(request):
+    return HttpResponse("This is about.")
+
+def services(request):
+    return HttpResponse("This is services.")
+
+def contact(request):
+    return HttpResponse("This is contactus.")
