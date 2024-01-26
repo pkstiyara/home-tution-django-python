4a84dcddbfc2ee3a326341f66480abe854e1fd9b
Pankaj Singh <pkstiyara@gmail.com>
2024-01-26 06:36:16 +0000

    Admin portal changed

--- gurgaonhometution/gurgaonhometution/urls.py
+++ gurgaonhometution/gurgaonhometution/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Gurgaon Home Tution Admin"
+admin.site.site_title = "Gurgaon Home Tution Admin Portal"
+admin.site.index_title = "Welcome to Gurgaon Home Tution Admin Portal"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('home.urls'))
